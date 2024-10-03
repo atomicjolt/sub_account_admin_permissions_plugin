8c9a42a8db89beb1277035e9d1e10a05abeccf46
Sean Collings <sean.collings@atomicjolt.com>
2024-10-03 12:04:00 -0600

    Added toggles for each of the permissions

--- lib/sub_account_admin_permissions_plugin/engine.rb
+++ lib/sub_account_admin_permissions_plugin/engine.rb
@@ -20,58 +20,78 @@ module SubAccountAdminPermissionsPlugin
         name: "OTC Sub-Account Admin Permissions",
         author: "Atomic Jolt",
         description: "Enables allowing sub-accounts to manage user login details",
-        version: "1.1.0",
+        version: "1.2.0",
         select_text: "OTC Sub-Account Admin Permisisons",
         settings_partial: 'sub_account_admin_permissions_plugin/plugin_settings'
       })
       if ActiveRecord::Base.connection.table_exists?('plugin_settings') && Canvas::Plugin.find(:otc_sub_account_admin_permissions).enabled?
+        @plugin ||= PluginSetting.find_by(name: "otc_sub_account_admin_permissions")
+
         # In development we have to force loading RoleOverride first, so the
         # default permissions are registered
         RoleOverride
-
         User
 
-        Permissions.register(
-          become_user: {
-            label: -> { "Act as users" },
-            label_v2: -> { "Users - act as" },
-            account_only: true,
-            true_for: %w[AccountAdmin],
-            available_to: %w[AccountAdmin AccountMembership],
-          }
-        )
-
-        module UserExtension
-          def can_masquerade?(masquerader, account)
-
-            # Get a list of all the accounts that the user's courses
-            # are contained within. This is used to determine if the
-            # user can masquerade as a user in a sub-account.
-            # This will result in n + 1 queries but it is cached
-            accounts = courses.map(&:associated_accounts).flatten.uniq
-
-            # Get all the accounts that the masquerader is an admin of
-            admin_accounts = masquerader.account_users.active.preload(:account).map(&:account)
-            intersection = accounts & admin_accounts
-
-            # If the masqureader is an admin in one of the parent accounts of
-            # the user's courses, then they can masquerade as the user
-            if intersection.length > 0
-              account = intersection.last
-
-              # We have to fake root_account privileges for the
-              # sub-account or Canvas will normally always return false
-              def account.root_account?
-                true
-              end
-            end
-
-            super(masquerader, account)
-          end
+        if @plugin.settings[:subaccount_manage_user_logins] == "true"
+          Permissions.register(
+            manage_user_logins: {
+              :label => lambda { I18n.t('permissions.manage_user_logins', "Modify login details for users") },
+              :label_v2 => lambda { I18n.t("Users - manage login details") },
+              :available_to => [
+                'AccountAdmin',
+                'AccountMembership'
+              ],
+              :account_only => true,
+              :true_for => [
+                'AccountAdmin'
+              ]
+            }
+          )
         end
 
-        class ::User < ActiveRecord::Base
-          prepend UserExtension
+        if @plugin.settings[:subaccount_become_user] == "true"
+          Permissions.register(
+            become_user: {
+              label: -> { "Act as users" },
+              label_v2: -> { "Users - act as" },
+              account_only: true,
+              true_for: %w[AccountAdmin],
+              available_to: %w[AccountAdmin AccountMembership],
+            }
+          )
+
+          module UserExtension
+            def can_masquerade?(masquerader, account)
+
+              # Get a list of all the accounts that the user's courses
+              # are contained within. This is used to determine if the
+              # user can masquerade as a user in a sub-account.
+              # This will result in n + 1 queries but it is cached
+              accounts = courses.map(&:associated_accounts).flatten.uniq
+
+              # Get all the accounts that the masquerader is an admin of
+              admin_accounts = masquerader.account_users.active.preload(:account).map(&:account)
+              intersection = accounts & admin_accounts
+
+              # If the masqureader is an admin in one of the parent accounts of
+              # the user's courses, then they can masquerade as the user
+              if intersection.length > 0
+                account = intersection.last
+
+                # We have to fake root_account privileges for the
+                # sub-account or Canvas will normally always return false
+                def account.root_account?
+                  true
+                end
+              end
+
+              super(masquerader, account)
+            end
+          end
+
+          class ::User < ActiveRecord::Base
+            prepend UserExtension
+          end
         end
       end
     end
